5c552fc4169c5f5d3e04c702f16226f016b60717
David Malmström <david.malmstrm@gmail.com>
2018-11-14 17:58:18 +0100

    add libs and skeleton code

---
+++ src/main.js
@@ -0,0 +1,28 @@
+
+class MapPlot {
+
+
+	constructor(svg_element_id) {
+		this.svg = d3.select('#' + svg_element_id);
+
+		// may be useful for calculating scales
+		const svg_viewbox = this.svg.node().viewBox.animVal;
+		this.svg_width = svg_viewbox.width;
+		this.svg_height = svg_viewbox.height;
+		}
+	}
+			
+
+function whenDocumentLoaded(action) {
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", action);
+	} else {
+		// `DOMContentLoaded` already fired
+		action();
+	}
+}
+
+whenDocumentLoaded(() => {
+	plot_object = new MapPlot('globe-plot');
+	// plot object is global, you can inspect it in the dev-console
+});
